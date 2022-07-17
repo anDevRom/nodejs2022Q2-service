3f144fdac43002d16c550bbe4b3ea6318e5a4045
Roman Andreev <andevrom@gmail.com>
2022-07-17 16:12:24 +0300

    feat: add api doc

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { parse } from 'yaml';
+import { readFileSync } from 'fs';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const docFile = readFileSync('./doc/api.yaml', 'utf-8');
+  SwaggerModule.setup('doc', app, parse(docFile));
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
